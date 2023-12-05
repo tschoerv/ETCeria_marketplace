376c8f0c2e6d4a27c9372f5f2e00394d7f74c8c0
tschoerv <tschoerv@protonmail.com>
2023-12-05 03:24:00 +0100

    trying to fix page refresh issue

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import Providers from "./providers";
 import "@rainbow-me/rainbowkit/styles.css";
 import Image from 'next/image';
 import Link from 'next/link';
+import Script from 'next/script';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,24 +16,25 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script src="/app/Scripts/registerServiceWorker.js" strategy="afterInteractive" />
       <body className={inter.className}>
-      <div className={"logo"}>
-      <Link href="/">
-          <Image
-      src="/logo.png"
-      width={450}
-      height={244}
-      alt="logo"
-      priority={true}
-      /></Link></div>
+        <div className={"logo"}>
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              width={450}
+              height={244}
+              alt="logo"
+              priority={true}
+            />
+          </Link>
+        </div>
         <Providers>{children}</Providers>
-
       </body>
     </html>
-    
   );
 }
+
